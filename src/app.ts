27a298dd8b2bc0f69a3e305b5b95867362eefda9
Daniel Wojdak <daniel.wojdak@impaqgroup.com>
2022-12-01 13:26:32 +0100

    Added cookies processing

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
 import express from 'express';
+
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 3000;
 
+app.use(cookieParser());
+
 app.get('/p', (req, res) => {
+  const cookie = req.cookies + req.cookies.name ? req.cookies.name : null;
   console.log(req.query);
-  res.send('Hello World! tests');
+  res.cookie('name', req.query && req.query.name ? req.query.name : 'Test' );
+  res.send(`Hello World!${cookie ? ' + ' + cookie : ''}`);
 });
 
 app.listen(port, () => {
