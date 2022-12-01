373df04a3acd7dbdea63f4e6a427251055e41ecd
Daniel Wojdak <daniel.wojdak@impaqgroup.com>
2022-12-01 14:37:02 +0100

    Added database library

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import { AceBase, DataReference, DataSnapshot } from 'acebase';
 
+const db = new AceBase('.');
 const cookieParser = require('cookie-parser');
 const app = express();
 const port = 3000;
@@ -9,10 +12,25 @@ app.use(cookieParser());
 app.get('/p', (req, res) => {
   const cookie = req.cookies + req.cookies.name ? req.cookies.name : null;
   console.log(req.query);
-  res.cookie('name', req.query && req.query.name ? req.query.name : 'Test' );
-  res.send(`Hello World!${cookie ? ' + ' + cookie : ''}`);
+  db.ref('users')
+    .push({
+        name: 'Ewout',
+        country: 'The Netherlands'
+    })
+    .then((userRef: DataReference) => {
+      console.log('key:', userRef.key);
+      userRef.get((snapshot: DataSnapshot) => {
+        if (snapshot.exists()) {
+          console.log('value:', snapshot.val());
+          res.cookie('name', req.query && req.query.name ? req.query.name : 'Test' );
+          res.send(`Hello World!${cookie ? ' + ' + cookie : ''}`);
+        }
+      });
+    });
 });
 
-app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
+db.ready(() => {
+  app.listen(port, () => {
+    return console.log(`Express is listening at http://localhost:${port}`);
+  });
 });
